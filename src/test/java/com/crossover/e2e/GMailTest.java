a9106bcb87f32dcca26d9da4b0ad84887af719c9
Akongnwi Che Devert <akongnwi.devert@cm.maviance.com>
2019-02-06 22:31:10 +0100

    Completed the test project

--- src/test/java/com/crossover/e2e/GMailTest.java
+++ src/test/java/com/crossover/e2e/GMailTest.java
@@ -2,6 +2,7 @@ package com.crossover.e2e;
 
 import java.io.File;
 import java.io.FileReader;
+import java.util.List;
 import java.util.Properties;
 import junit.framework.TestCase;
 import org.junit.Test;
@@ -21,8 +22,8 @@ public class GMailTest extends TestCase {
         driver = new ChromeDriver();
     }
 
-    public void tearDown() throws Exception {
-        //driver.quit();
+    public void tearDown() {
+        driver.quit();
     }
 
     /*
@@ -34,55 +35,76 @@ public class GMailTest extends TestCase {
         // emailSubject and emailbody to be used in this unit test.
         String emailSubject = properties.getProperty("email.subject");
         String emailBody = properties.getProperty("email.body");
-        String emailLable = properties.getProperty("email.label");
+        String emailLabel = "Social";
         // username and password to be used for this unit test
         String password = properties.getProperty("password");
         String username = properties.getProperty("username");
 
+        // Login to Gmail
         driver.get("https://mail.google.com/");
         Thread.sleep(2000);
-
         WebElement userElement = driver.findElement(By.id("identifierId"));
         userElement.sendKeys(username);
-
         driver.findElement(By.id("identifierNext")).click();
         Thread.sleep(2000);
-
         WebElement passwordElement = driver.findElement(By.name("password"));
         passwordElement.sendKeys(password);
         driver.findElement(By.id("passwordNext")).click();
         Thread.sleep(2000);
 
+        // Compose an email from subject and body as mentioned in test.properties
         WebElement composeElement = driver.findElement(By.xpath("//*[@role='button' and text()='Compose']"));
         composeElement.click();
-        Thread.sleep(1500);
-
+        Thread.sleep(2000);
         driver.findElement(By.name("to")).clear();
         driver.findElement(By.name("to")).sendKeys(String.format("%s@gmail.com", username));
-
         driver.findElement(By.name("subjectbox")).clear();
         driver.findElement(By.name("subjectbox")).sendKeys(emailSubject);
-
         driver.findElement(By.xpath("//*[@role='textbox']")).clear();
         driver.findElement(By.xpath("//*[@role='textbox']")).sendKeys(emailBody);
         Thread.sleep(1000);
 
+        // Label email as "Social"
         driver.findElement(By.xpath("//*[@role='button' and @aria-label='More options']")).click();
         WebElement labelBtn =  driver.findElement(By.xpath("//*[@role='menuitem']//*[text()='Label']"));
         Actions action = new Actions(driver);
         action.moveToElement(labelBtn).perform();
-        Thread.sleep(500);
-
-        driver.findElement(By.xpath("//input[@ignoreesc='true']")).sendKeys(emailLable);
+        Thread.sleep(1000);
+        driver.findElement(By.xpath("//input[@ignoreesc='true']")).sendKeys(emailLabel);
         driver.findElement(By.xpath("//input[@ignoreesc='true']")).sendKeys(Keys.ENTER);
+
+        // Send the email to the same account which was used to login
         driver.findElement(By.xpath("//*[@role='button' and text()='Send']")).click();
-        Thread.sleep(4000);
 
+        // Wait for the email to arrive in the Inbox
+        Thread.sleep(5000);
         driver.findElement(By.xpath("//*[@role='tab' and @aria-label='Social']")).click();
-        Thread.sleep(200);
+        Thread.sleep(2000);
 
+        // Mark email as starred
         driver.findElement(By.xpath(String.format("//*[@name='me' and @email='%s@gmail.com']/parent::span/parent::div/parent::td/parent::tr//span[@title='Not starred']", username))).click();
-        driver.findElement(By.xpath(String.format("//*[@email='%s@gmail.com' and @data-hovercard-owner-id='23']", username))).click();
-        Thread.sleep(500);
+        Thread.sleep(3000);
+
+        // Open the received email
+        driver.findElement(By.xpath("//*[@role='tab' and @aria-label='Social']")).click();
+        Thread.sleep(3000);
+        List<WebElement> email = driver.findElements(By.cssSelector("div.xT>div.y6>span>span"));
+        for(WebElement myEmail : email){
+            if(myEmail.getText().equals(emailSubject)){
+                myEmail.click();
+                break;
+            }
+        }
+        Thread.sleep(2000);
+
+        // Verify email came under proper Label i.e. "Social"
+        driver.findElement(By.xpath("//div[@title='Search for all messages with label Social']")).isDisplayed();
+
+        // Verify the subject and body of the received email
+        WebElement emailSubjectElement = driver.findElement(By.cssSelector(".nH>.ha>h2"));
+        assertEquals(emailSubject, emailSubjectElement.getText());
+        WebElement emailBodyElement = driver.findElement(By.xpath("//*[@class='gs']//div[@dir='ltr']"));
+        assertEquals(emailBody, emailBodyElement.getText());
+
     }
 }
